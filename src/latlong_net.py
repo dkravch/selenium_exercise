2f3c8b939ff346146d06e7b9015d0f2ecd43335c
Denys Kravchenko <dkravchenko@truveris.com>
2023-06-14 12:30:50 +0300

    Add multi-check

--- src/latlong_net.py
+++ src/latlong_net.py
@@ -2,6 +2,7 @@ from .base import BasePage, singleton
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 @singleton
@@ -30,10 +31,10 @@ class LatLongNetPage(BasePage):
 
     def text_appears_in_lat_long_output(self, text):
         try:
-            element = WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.text_to_be_present_in_element(self.elements_map['lat_long_output'], text)
             )
             return True
-        except Exception:
+        except TimeoutException:
             return False
 
--- tests/test_web.py
+++ tests/test_web.py
@@ -1,10 +1,20 @@
 import time
+from pytest_check import check
 
 
-def test_dummy_local(lat_long_page):
+def test_lat_long_net_happy(lat_long_page):
     lat_long_page.open()
     lat_long_page.submit_city_name('Kyiv')
     assert lat_long_page.text_appears_in_lat_long_output("(50.447731, 30.542721)")
+
+
+def test_lat_long_net_not_exists(lat_long_page):
+
+    lat_long_page.open()
+    lat_long_page.submit_city_name('Newerlendfromnowhere')
+    check.equal(lat_long_page.lat_long_output.text, '(0.000000, 0.000000)')
+    check.equal(lat_long_page.latitude_output.get_attribute('value'),  '0.000000')  # Get value from input field
+    check.equal(lat_long_page.longitude_output.get_attribute('value'), '0.000000')  # Get value from input field
 
 
 def test_dummy_remote(remote_driver):
